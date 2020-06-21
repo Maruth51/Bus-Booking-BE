32a87cd7ba100444b5d925f1ab17695754d6ad94
Maruth51 <45478165+Maruth51@users.noreply.github.com>
2020-06-21 18:16:30 +0530

    index-update

--- src/routes/indexRouter.js
+++ src/routes/indexRouter.js
@@ -2,21 +2,25 @@ var express = require("express");
 var router = express.Router();
 const City = require("../models/city");
 
-router.get("/:query", async (req, res) => {
-  try {
-    const { query } = req.params;
-    const key = new RegExp("^" + query);
-    const city = await City.find(
-      {
-        City: { $regex: key, $options: "i" }
-      },
-      { City: 1, _id: 0 }
-    ).limit(10);
-    const arrCity = city.map((ele, index) => {
-      return ele.City;
-    });
-    res.send(arrCity);
-  } catch {}
-});
+router
+  .get("/", (req, res) => {
+    res.send("Home Page");
+  })
+  .get("/:query", async (req, res) => {
+    try {
+      const { query } = req.params;
+      const key = new RegExp("^" + query);
+      const city = await City.find(
+        {
+          City: { $regex: key, $options: "i" }
+        },
+        { City: 1, _id: 0 }
+      ).limit(10);
+      const arrCity = city.map((ele, index) => {
+        return ele.City;
+      });
+      res.send(arrCity);
+    } catch {}
+  });
 
 module.exports = router;
